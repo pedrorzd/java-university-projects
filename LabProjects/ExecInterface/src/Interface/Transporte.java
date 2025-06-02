9099f8d9548b60850b63a1adcbe6930bdab6400c
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2025-06-01 22:33:57 -0300

    atualizaçao de horario de entrega

--- LabProjects/ExecInterface/src/Interface/Transporte.java
+++ LabProjects/ExecInterface/src/Interface/Transporte.java
@@ -1,7 +1,15 @@
 package Interface;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public interface Transporte {
+    Date data = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
     public static void entrega(String appUsado, String idEntrega, String nomeEntegador, String dataEntrega, String veiculoEntregador, String enderecoEntrega, String distancia) {
+
         System.out.println();
         System.out.println("Entrega Inciada");
         System.out.println(
@@ -13,6 +21,9 @@ public interface Transporte {
                 + "\n Veiculo: " + veiculoEntregador
                 + "\n Distancia: " + distancia
                 );
+
+        System.out.println("CHEGANDO NO ENDEREÇO: " + enderecoEntrega);
+        System.out.println("ENTREGA CONCLUIDA AS " + sdf.format(data));
         System.out.println();
 
     }
